d7ee7ae2a0925eee332be2dd6f4e9e6d94e57a09
hamed17n <hamed.kabiri@gandom.co>
2018-05-29 16:11:54 +0430

    add textarea component and storybook

---
+++ src/components/textare/index.tsx
@@ -0,0 +1,96 @@
+import * as React from 'react';
+import * as classNames from 'classnames';
+
+import './styles.scss';
+
+export interface InputProps extends React.HTMLProps<HTMLTextAreaElement> {
+  errorMessage?: string;
+  forceDisplayError?: boolean;
+  validate?: (value: boolean | string | number) => boolean;
+  title?: string;
+}
+
+export interface InputState {
+  touched: boolean;
+  value: any;
+}
+
+class Input extends React.Component<InputProps, InputState> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      touched: false,
+      value: props.value || '',
+    };
+  }
+
+  componentWillReceiveProps(nextProps: InputProps) {
+    if (
+      nextProps.value !== this.props.value &&
+      nextProps.value !== this.state.value
+    ) {
+      this.setState({ value: nextProps.value });
+    }
+  }
+
+  handleInput = e => {
+    if (e.target.value !== this.state.value) {
+      if (!this.state.touched) {
+        this.setState({ touched: true, value: e.target.value });
+      } else {
+        this.setState({ value: e.target.value });
+      }
+    }
+
+    if (this.props.onInput) {
+      this.props.onInput(e);
+    }
+  }
+
+  render() {
+    if (this.props.errorMessage && !this.props.validate) {
+      throw new TypeError(
+        'Please provide either both errorMessage and ' +
+        'validate or non of them.',
+      );
+    }
+
+    const {
+      errorMessage,
+      forceDisplayError,
+      validate,
+      title,
+      ...props,
+    } = this.props;
+
+    const hasError = (
+      errorMessage &&
+      (forceDisplayError || this.state.touched) &&
+      !validate(this.state.value)
+    );
+
+    const className = classNames('textarea-container', {
+      error: hasError,
+    });
+
+    return (
+      <div className={className}>
+        {title && (
+          <label htmlFor={props.id || ''}>{title}</label>
+        )}
+        <textarea
+          cols={this.props.cols}
+          rows={this.props.rows}
+          value={this.state.value}
+          onChange={this.handleInput}
+          {...props} />
+        {hasError && (
+          <span className="error">{errorMessage}</span>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Input;
