28c59b08b0bf60b58e7580d28786865d06b254c0
hamed17n <hamed17n@yahoo.com>
2018-10-28 11:42:37 +0330

    feat(global): add new webpack config

---
+++ src/components/menu/index.ts
@@ -0,0 +1,5 @@
+export { default as Avatar } from './avatar';
+export { default as Header } from './header';
+export { default as Item } from './item';
+export { default as Link } from './link';
+export { default as Status } from './status';
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.ts"),
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.js",
+    library: "Milingo",
+    libraryTarget: "commonjs"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/react"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "resolve-url-loader",
+            options: {
+              debug: false
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [require("autoprefixer")],
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              includePaths: ["./node_modules"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: "awesome-typescript-loader"
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
+        ]
+      }
+    ]
+  }
+};
