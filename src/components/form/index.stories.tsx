0fa4ec9a7b2f609cbbc43cbb41d6f7d792fd4544
hamed17n <hamed17n@yahoo.com>
2018-10-31 11:56:25 +0330

    feat: add couple column test+ fix checkbox direction

--- src/components/form/index.stories.tsx
+++ src/components/form/index.stories.tsx
@@ -4,6 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { Form, FormGroup } from '.';
 import Input from '../input';
 import Textarea from '../textarea';
+import Checkbox from '../checkbox';
 import Radio from '../radiobutton';
 import Button from '../button';
 
@@ -27,6 +28,37 @@ storiesOf('Form', module)
         <FormGroup title="عنوان">
           <Radio checked> رفت وبرگشت</Radio>
           <Radio> رفت وبرگشت</Radio>
+        </FormGroup>
+        <Button> انصراف</Button>
+        <Button primary> ارسال</Button>
+      </Form>
+    </div>
+  ))
+  .addWithJSX('Couple column', () => (
+    <div
+      style={{
+        width: '550px',
+        maxWidth: '100%',
+        backgroundColor: '#ffffff',
+        padding: '30px',
+      }}>
+      <Form title="عنوان فرم" description="توضیحات فرم">
+        <FormGroup singleRow>
+          <Input title="عنوان" />
+          <Input title="عنوان" />
+        </FormGroup>
+        <FormGroup>
+          <Textarea title="عنوان" placeholder="متن" rows={4} />
+        </FormGroup>
+        <FormGroup singleRow>
+          <FormGroup title="عنوان">
+            <Radio checked> رفت وبرگشت</Radio>
+            <Radio> رفت وبرگشت</Radio>
+          </FormGroup>
+          <FormGroup title="عنوان">
+            <Checkbox checked> رفت وبرگشت</Checkbox>
+            <Checkbox> رفت وبرگشت</Checkbox>
+          </FormGroup>
         </FormGroup>
         <Button> انصراف</Button>
         <Button primary> ارسال</Button>
