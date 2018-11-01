fd5a1f2148bba22a9cf6dccda8b9fe64c8ac428a
hamed17n <hamed17n@yahoo.com>
2018-11-01 14:35:24 +0330

    feat: add counter logic to CounterButton component

--- src/components/counter-button/index.tsx
+++ src/components/counter-button/index.tsx
@@ -5,18 +5,66 @@ import './styles.scss';
 
 export interface CounterButtonProps extends React.HTMLProps<HTMLButtonElement> {
   className?: string;
+  onIncrease?: () => {};
+  onDecrease?: () => {};
+  onCountUpdate?: (newCount: number) => {};
+  acceptNegative?: boolean;
 }
 
-class CounterButton extends React.Component<CounterButtonProps, {}> {
+export interface CounterButtonStates {
+  count: number;
+}
+
+class CounterButton extends React.Component<
+  CounterButtonProps,
+  CounterButtonStates
+> {
+  state = {
+    count: 0,
+  };
+
+  updateCount(count) {
+    this.setState({ count });
+    if (this.props.onCountUpdate) {
+      this.props.onCountUpdate(count);
+    }
+  }
+
+  handleDecrease() {
+    if (!this.props.acceptNegative && this.state.count === 0) {
+      return;
+    }
+
+    const count = this.state.count - 1;
+    this.updateCount(count);
+
+    if (this.props.onDecrease) {
+      this.props.onDecrease();
+    }
+  }
+
+  handleIncrease() {
+    const count = this.state.count + 1;
+    this.updateCount(count);
+
+    if (this.props.onIncrease) {
+      this.props.onIncrease();
+    }
+  }
+
   render() {
     const { className } = this.props;
     const componentClassname = classNames('counter-button-wrapper', className);
     return (
       <div className={componentClassname}>
-        <div className="button-wrapper">
+        <div
+          className="button-wrapper"
+          onClick={this.handleIncrease.bind(this)}>
           <span className="icon material-icons">add</span>
         </div>
-        <div className="button-wrapper">
+        <div
+          className="button-wrapper"
+          onClick={this.handleDecrease.bind(this)}>
           <span className="icon material-icons">remove</span>
         </div>
       </div>
