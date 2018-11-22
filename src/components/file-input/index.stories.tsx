3d0db76e0c884486dad359fc38bf6becc4aec4f2
hamed17n <hamed17n@yahoo.com>
2018-11-22 12:53:54 +0330

    fix: file input styles

--- src/components/file-input/index.stories.tsx
+++ src/components/file-input/index.stories.tsx
@@ -2,61 +2,60 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import FileInput from '.';
 
-const testFilesDictionary = { 0:new File([""], 'filename.jpg'), length:1 }
+const testFilesDictionary = { 0: new File([''], 'filename.jpg'), length: 1 };
 
 const testMultipleFilesDictionary = {
-  0: new File([""], 'filename1.jpg'),
-  1: new File([""], 'filename2.jpg'),
-  2: new File([""], 'filename3.jpg'),
+  0: new File([''], 'filename1.jpg'),
+  1: new File([''], 'filename2.jpg'),
+  2: new File([''], 'filename3.jpg'),
   length: 3,
-}
+};
 
 const loadingTestState = {
-  0:{
+  0: {
     loading: true,
     progress: 70,
-  }
-}
+  },
+};
 
 const errorTestState = {
-  0:{
-    message:'حجم فایل شما بیش از حد مجاز است.',
-  }
-}
+  0: {
+    message: 'حجم فایل شما بیش از حد مجاز است.',
+  },
+};
 
 const tryTestState = {
-  0:{
-    message:'فایل بارگذاری نشد.',
+  0: {
+    message: 'فایل بارگذاری نشد.',
     tryAgain: true,
-  }
-}
+  },
+};
 
 storiesOf('File input', module)
   .addDecorator(story => (
-    <div className="story-container">
-      {story()}
-    </div>
+    <div className="button-story-container">{story()}</div>
   ))
   .addWithJSX('Normal', () => (
-    <div style={{
-      width: '450px',
-      maxWidth: '100%',
-      backgroundColor: '#ffffff',
-      padding: '40px',
-    }}>
+    <div
+      style={{
+        width: '450px',
+        maxWidth: '100%',
+        backgroundColor: '#ffffff',
+        padding: '40px',
+      }}>
       <FileInput />
-      <FileInput disabled/>
+      <FileInput disabled />
       <FileInput files={testFilesDictionary} />
-      <FileInput  files={testFilesDictionary} disabled/>
+      <FileInput files={testFilesDictionary} disabled />
       <FileInput states={loadingTestState} files={testFilesDictionary} />
       <FileInput
-        validate={()=>false}
+        validate={() => false}
         states={errorTestState}
         files={testFilesDictionary}
         forceDisplayError
       />
       <FileInput
-        validate={()=>false}
+        validate={() => false}
         states={tryTestState}
         files={testFilesDictionary}
         forceDisplayError
@@ -64,12 +63,13 @@ storiesOf('File input', module)
     </div>
   ))
   .addWithJSX('Multiple', () => (
-    <div style={{
-      width: '450px',
-      maxWidth: '100%',
-      backgroundColor: '#ffffff',
-      padding: '40px',
-    }}>
+    <div
+      style={{
+        width: '450px',
+        maxWidth: '100%',
+        backgroundColor: '#ffffff',
+        padding: '40px',
+      }}>
       <FileInput multiple files={testMultipleFilesDictionary} />
     </div>
   ));
