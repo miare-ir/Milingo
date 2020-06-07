bf68e3381a6dd6d56d8fd348af131c7b3b85eadb
Shima Sherafati <sherafati.sh@gmail.com>
2020-06-07 17:29:12 +0430

    fix(global): Add docs and dist folder

---
+++ docs/precache-manifest.3cfea532ff709851c48142d33bc853db.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ffd31339cd4f78da92",
+    "url": "0.styles.css"
+  },
+  {
+    "revision": "484eb0cbec68fcd5f83b0d56f122f578",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.b693e8ddefce6cb1df74.bundle.js"
+  },
+  {
+    "url": "runtime~main.b693e8ddefce6cb1df74.bundle.js"
+  },
+  {
+    "revision": "b9ffd31339cd4f78da92",
+    "url": "static/css/main.a1b471d3.chunk.css"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "beb4c4b35304a86b98fa6e95a22f0ffe",
+    "url": "static/media/arrow.beb4c4b3.svg"
+  },
+  {
+    "revision": "748f32d92cf483f2eb5b46ca95fc4e40",
+    "url": "static/media/iransans-bold.748f32d9.woff"
+  },
+  {
+    "revision": "21996b67bae94dde1afea1865865f8ce",
+    "url": "static/media/iransans-medium.21996b67.woff"
+  },
+  {
+    "revision": "7e5c1289975b413a1f109bc490d7508c",
+    "url": "static/media/iransans-regular.7e5c1289.woff"
+  },
+  {
+    "revision": "4843525fe3bf55010a24f26dd8575779",
+    "url": "static/media/notification-error.4843525f.svg"
+  },
+  {
+    "revision": "da1566f97f7cc61284a553a75d72d1e4",
+    "url": "static/media/notification-hint.da1566f9.svg"
+  },
+  {
+    "revision": "3f9f471c3a7f0da2f0f732ca65ab87e4",
+    "url": "static/media/notification-success.3f9f471c.svg"
+  },
+  {
+    "revision": "fa3fd3bf0033e7fbc677385fde4b959c",
+    "url": "static/media/notification-warning.fa3fd3bf.svg"
+  },
+  {
+    "url": "vendors~main.b693e8ddefce6cb1df74.bundle.js"
+  },
+  {
+    "url": "vendors~main.b693e8ddefce6cb1df74.bundle.js.LICENSE.txt"
+  }
+]);
