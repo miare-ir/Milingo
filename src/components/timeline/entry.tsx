1c6404ac2ceea09824064d9629df634748b303d7
qafoori <qafoori.hr@gmail.com>
2022-09-11 18:04:22 +0430

    chore(timline): Change the way of using className

--- src/components/timeline/entry.tsx
+++ src/components/timeline/entry.tsx
@@ -23,10 +23,13 @@ const TimelineEntry: React.SFC<TimelineEntryProps> = ({
 }: TimelineEntryProps) => (
   <div
     {...props}
-    className={classNames('timeline', {
-      clickable: clickable,
-      clicked: !!time && clickable && !loading,
-    }).concat(props.className ? ' ' + props.className : '')}>
+    className={classNames(
+      `timeline ${props.className ? props.className : ''}`,
+      {
+        clickable: clickable,
+        clicked: !!time && clickable && !loading,
+      },
+    )}>
     <div className="entry-title-container">
       <div className="entry-time-container">
         <div className="entry-time">{loading ? <Loader disabled /> : time}</div>
