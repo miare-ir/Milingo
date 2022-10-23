f88eedc5bca9c169c9f4d8390b994bd71d3e09b7
zahra mirkazemi <zahramirkazemi1997@gmail.com>
2022-10-23 22:17:32 +0330

    fix(accordion): Remove header from chat component

--- src/components/chat/index.stories.tsx
+++ src/components/chat/index.stories.tsx
@@ -18,123 +18,58 @@ const handelSubmitMessage = (id: number, message: string): void =>
 
 const handelResolve = (id: number): void => alert(`ایشیو ${id} بسته شد`);
 
-export const Default = (): JSX.Element => {
-  const [isOpen, setIsOpen] = React.useState(true);
+export const Default = (): JSX.Element => (
+  <Chat
+    chatId={issue.id}
+    resolveBtnTitle={'رسیدگی شد'}
+    onSubmitMessage={handelSubmitMessage}
+    isSendingMessage={false}
+    canSubmitMessage
+    isResolved={false}
+    resolve={handelResolve}>
+    {issue.messages.map(({ message, id, sender_type, created_at }) => (
+      <Message
+        id={id}
+        key={id}
+        message={message}
+        isRight={sender_type !== 'staff'}
+        created_at={created_at}
+      />
+    ))}
+  </Chat>
+);
 
-  return (
-    <Chat
-      chatId={issue.id}
-      title={issue.problem.title}
-      reporter_type={issue.reporter_type}
-      username={
-        issue.reported_by.first_name + ' ' + issue.reported_by.last_name
-      }
-      isOpen={isOpen}
-      resolveBtnTitle={'رسیدگی شد'}
-      setIsOpen={() => setIsOpen(!isOpen)}
-      onSubmitMessage={handelSubmitMessage}
-      isSendingMessage={false}
-      canSubmitMessage
-      hasHeader
-      isResolved={false}
-      resolve={handelResolve}>
-      {issue.messages.map(({ message, id, sender_type, created_at }) => (
-        <Message
-          id={id}
-          key={id}
-          message={message}
-          isRight={sender_type !== 'staff'}
-          created_at={created_at}
-        />
-      ))}
-    </Chat>
-  );
-};
+export const WithoutMessage = (): JSX.Element => (
+  <Chat
+    chatId={issue.id}
+    resolveBtnTitle={'رسیدگی شد'}
+    onSubmitMessage={handelSubmitMessage}
+    isSendingMessage={false}
+    canSubmitMessage
+    isResolved={false}
+    resolve={handelResolve}>
+    {[].map(({ message, id, sender_type, created_at }) => (
+      <Message
+        id={id}
+        key={id}
+        message={message}
+        isRight={sender_type !== 'staff'}
+        created_at={created_at}
+      />
+    ))}
+  </Chat>
+);
 
-export const WithoutMessage = (): JSX.Element => {
-  const [isOpen, setIsOpen] = React.useState(true);
-
-  return (
-    <Chat
-      chatId={issue.id}
-      title={issue.problem.title}
-      reporter_type={issue.reporter_type}
-      username={
-        issue.reported_by.first_name + ' ' + issue.reported_by.last_name
-      }
-      isOpen={isOpen}
-      resolveBtnTitle={'رسیدگی شد'}
-      setIsOpen={() => setIsOpen(!isOpen)}
-      onSubmitMessage={handelSubmitMessage}
-      isSendingMessage={false}
-      canSubmitMessage
-      hasHeader
-      isResolved={false}
-      resolve={handelResolve}>
-      {[].map(({ message, id, sender_type, created_at }) => (
-        <Message
-          id={id}
-          key={id}
-          message={message}
-          isRight={sender_type !== 'staff'}
-          created_at={created_at}
-        />
-      ))}
-    </Chat>
-  );
-};
-
-export const WithoutSubmitMessage = (): JSX.Element => {
-  const [isOpen, setIsOpen] = React.useState(true);
-
-  return (
-    <Chat
-      chatId={issue.id}
-      title={issue.problem.title}
-      reporter_type={issue.reporter_type}
-      username={
-        issue.reported_by.first_name + ' ' + issue.reported_by.last_name
-      }
-      isOpen={isOpen}
-      setIsOpen={() => setIsOpen(!isOpen)}
-      canSubmitMessage={false}
-      hasHeader
-      isResolved={false}>
-      {issue.messages.map(({ message, id, sender_type, created_at }) => (
-        <Message
-          id={id}
-          key={id}
-          message={message}
-          isRight={sender_type === 'staff'}
-          created_at={created_at}
-        />
-      ))}
-    </Chat>
-  );
-};
-
-export const WithoutHeader = (): JSX.Element => {
-  const [isOpen, setIsOpen] = React.useState(true);
-
-  return (
-    <Chat
-      chatId={issue.id}
-      title={issue.problem.title}
-      isOpen={isOpen}
-      setIsOpen={() => setIsOpen(!isOpen)}
-      isSendingMessage={false}
-      resolveBtnTitle={'رسیدگی شد'}
-      onSubmitMessage={handelSubmitMessage}
-      isResolved>
-      {issue.messages.map(({ message, id, sender_type, created_at }) => (
-        <Message
-          id={id}
-          key={id}
-          message={message}
-          isRight={sender_type === 'staff'}
-          created_at={created_at}
-        />
-      ))}
-    </Chat>
-  );
-};
+export const WithoutSubmitMessage = (): JSX.Element => (
+  <Chat chatId={issue.id} canSubmitMessage={false} isResolved={false}>
+    {issue.messages.map(({ message, id, sender_type, created_at }) => (
+      <Message
+        id={id}
+        key={id}
+        message={message}
+        isRight={sender_type === 'staff'}
+        created_at={created_at}
+      />
+    ))}
+  </Chat>
+);
--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -2,21 +2,14 @@ import * as React from 'react';
 
 import * as classNames from 'classnames';
 
-import closeIcon from '../../assets/icon/close.svg';
 import './styles.scss';
 import SubmitForm from './submit-form';
 
 export interface ChatProps extends React.HTMLAttributes<HTMLDivElement> {
-  title?: string;
   chatId: number;
-  username?: string;
-  reporter_type?: string;
-  isOpen?: boolean;
-  setIsOpen: () => void;
   onSubmitMessage?: (id: number, text: string) => void;
   isSendingMessage?: boolean;
   canSubmitMessage?: boolean;
-  hasHeader?: boolean;
   isResolved?: boolean;
   resolveBtnTitle?: string;
   resolve?: (id: number) => void;
@@ -24,44 +17,20 @@ export interface ChatProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const Chat = ({
-  title,
   chatId,
-  username,
-  reporter_type,
-  isOpen,
-  setIsOpen,
   onSubmitMessage,
   isSendingMessage,
   canSubmitMessage,
-  hasHeader,
   isResolved,
   resolveBtnTitle,
   resolve,
   children,
   ...rest
 }: ChatProps): JSX.Element => {
-  const [toggle, setToggle] = React.useState(true);
-
-  const componentClassNames = classNames(
-    'chat-container',
-    isOpen ? 'open' : '',
-    toggle ? 'active' : '',
-    rest.className,
-  );
+  const componentClassNames = classNames('chat-container', rest.className);
 
   return (
     <div {...rest} className={componentClassNames}>
-      {hasHeader && (
-        <div
-          className={`chat-header ${reporter_type}-issue`}
-          onClick={() => setToggle(currentToggle => !currentToggle)}>
-          <div className="chat-info">
-            <div className="chat-title">{title}</div>
-            <div className="chat-username">{username}</div>
-          </div>
-          <img src={closeIcon} onClick={setIsOpen} />
-        </div>
-      )}
       <div className="chat-content">{children}</div>
       {canSubmitMessage && (
         <SubmitForm
