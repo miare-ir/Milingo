3675b02034237c514602a0ef901d0b07f8939769
zahra mirkazemi <zahramirkazemi1997@gmail.com>
2022-11-01 14:21:35 +0330

    fix(tab): Rename selectedtab to selectedTab in tab content

--- src/components/tab/tab-content.tsx
+++ src/components/tab/tab-content.tsx
@@ -6,16 +6,16 @@ import './styles.scss';
 export interface TabContentProps extends React.HTMLProps<HTMLDivElement> {
   className?: string;
   children?: JSX.Element[] | JSX.Element;
-  selectedtab: number | string;
+  selectedTab: number | string;
 }
 class TabContent extends React.Component<TabContentProps> {
   render(): React.ReactNode {
-    const { className, children, selectedtab, ...props } = this.props;
+    const { className, children, selectedTab, ...props } = this.props;
     const componentClassName = classNames('tab-content-wrapper', className);
 
     const renderSelectedChild = (): JSX.Element => {
       for (const child of children as JSX.Element[]) {
-        if (child.props.tabId === selectedtab) {
+        if (child.props.tabId === selectedTab) {
           return child;
         }
       }
