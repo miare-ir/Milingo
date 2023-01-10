83b4e4943ae1f4125e457e9cc569edc93965b569
Shima Sherafati <sherafati.sh@gmail.com>
2023-01-10 18:28:02 +0330

    update milingo

--- docs/precache-manifest.e0e8bf8cb1e9348ed33d4d880452f0ed.js
+++ docs/precache-manifest.e0e8bf8cb1e9348ed33d4d880452f0ed.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f96422fcfb6b78aa9964",
+    "revision": "f1d32c4a822aed633a09",
     "url": "0.styles.css"
   },
   {
-    "revision": "efa37e7cdc82b7d88f4c93727a02c76b",
+    "revision": "b524ee822eef15f879d04690fc716708",
     "url": "iframe.html"
   },
   {
-    "revision": "f96422fcfb6b78aa9964",
-    "url": "main.0d95bc72.iframe.bundle.js"
+    "revision": "f1d32c4a822aed633a09",
+    "url": "main.04d4f867.iframe.bundle.js"
   },
   {
     "revision": "c156b6caa0afff344398",
     "url": "runtime~main.248e4f1a.iframe.bundle.js"
   },
   {
-    "revision": "f96422fcfb6b78aa9964",
-    "url": "static/css/main.ef7b9e5a.chunk.css"
+    "revision": "f1d32c4a822aed633a09",
+    "url": "static/css/main.c2d883cb.chunk.css"
   },
   {
     "revision": "3adfaf1dba68cdd44358c4f4f711fdc8",
@@ -108,11 +108,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "static/media/stripes.0779ac7f.svg"
   },
   {
-    "revision": "273bf8adf420593edad2",
-    "url": "vendors~main.6c5f288a.iframe.bundle.js"
+    "revision": "eb6240714a20023c2d41",
+    "url": "vendors~main.a140a79a.iframe.bundle.js"
   },
   {
     "revision": "f08e38d2555c7d9a08544165a3e6fd36",
-    "url": "vendors~main.6c5f288a.iframe.bundle.js.LICENSE.txt"
+    "url": "vendors~main.a140a79a.iframe.bundle.js.LICENSE.txt"
   }
 ]);
