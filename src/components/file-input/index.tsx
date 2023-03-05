088f2c2f73f068a63775d57c9cc13ca25f71cb00
Zahra Mirkazemi <zahramirkazemi1992@gmail.com>
2023-03-05 22:54:39 +0330

    fix(file-input): Change type of receive buttons

--- src/components/file-input/index.tsx
+++ src/components/file-input/index.tsx
@@ -26,8 +26,8 @@ export interface FileInputProps extends React.HTMLProps<HTMLInputElement> {
   states?: StatesDictionary;
   title?: string;
   validate?: (value: File[]) => boolean;
-  children?: string;
-  tryAgainText?: string;
+  children?: string | React.ReactNode;
+  tryAgainText?: string | React.ReactNode;
   inputRef?: React.RefObject<HTMLInputElement>;
 }
 
