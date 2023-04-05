2f0829c6dbe1c35a4eb07a6027ed323bcd5d94b6
qafoori <qafoori.hr@gmail.com>
2023-04-05 11:59:30 +0330

    feat(global): Export all types of components from their modules

--- src/components/collection/index.tsx
+++ src/components/collection/index.tsx
@@ -1,2 +1,5 @@
-export { default as Collection } from './collection';
-export { default as CollectionCard } from './collection-card';
+export { default as Collection, CollectionProps } from './collection';
+export {
+  default as CollectionCard,
+  CollectionCardProps,
+} from './collection-card';
