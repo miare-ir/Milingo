0574c83fa7b7c3a7ff88db8adea791f8f27c719a
DelaramSheikhbahaie <sheikhbahaiework@gmail.com>
2024-02-13 01:44:07 +0330

    feat(stepper): fix stepper in mobile

--- src/components/stepper/index.tsx
+++ src/components/stepper/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from 'react';
 import './styles.scss';
 
 export enum StepsStatus {
@@ -27,48 +27,66 @@ const Stepper = (props: StepperProps): JSX.Element => {
 
   const getStepIndex = (step): number => props.steps.indexOf(step);
   const isFirstStep = (step): boolean => getStepIndex(step) === 0;
-  const isLastStep = (step): boolean =>getStepIndex(step) + 1 === props.steps.length
+  const isLastStep = (step): boolean =>
+    getStepIndex(step) + 1 === props.steps.length;
   const isOnlyStep = (): boolean => props.steps.length === 1;
-   
 
   const renderMobileStepper = (): JSX.Element => {
     const doingStep = props.steps.find(
       step => step.status === StepsStatus.Doing,
     );
     const doingStepIndex = getStepIndex(doingStep);
-    const steps = props.steps.slice(doingStepIndex - 1, doingStepIndex + 2);
+    const steps =
+      !isLastStep(doingStep) && !isFirstStep(doingStep)
+        ? props.steps.slice(doingStepIndex - 1, doingStepIndex + 2)
+        : isFirstStep(doingStep)
+        ? props.steps.slice(doingStepIndex, doingStepIndex + 3)
+        : isLastStep(doingStep) &&
+          props.steps.slice(doingStepIndex - 2, doingStepIndex + 1);
+
+    const isMoreThanThreeSteps = props.steps.length > 3;
+
     return (
       <>
-        {steps.map((step, index) => (
-          <div
-            className={`step-container not-in-desktop ${getClassName(
-              step.status,
-            )}`}
-            key={index}>
-            {!isFirstStep(step) && !isOnlyStep() && index === 0 && (
-              <hr className="line" />
-            )}
-            <div className="step">{step.title}</div>
-            {!isLastStep(step) && !isOnlyStep() && <hr className="line" />}
-          </div>
-        ))}
+        {isMoreThanThreeSteps
+          ? steps.map((step, index) => (
+              <div
+                className={`step-container not-in-desktop ${getClassName(
+                  step.status,
+                )}`}
+                key={index}>
+
+                {index===0 && !isFirstStep(step) && <hr className={`line start-line ${getClassName(
+                  props.steps[getStepIndex(step)-1]?.status,
+                )}`}/>}
+                <div className="step">{step.title}</div>
+                {!isLastStep(step) && <hr className="line" />}
+              </div>
+            ))
+          : !isMoreThanThreeSteps &&
+            props.steps.map((step, index) => (
+              <div
+                key={index}
+                className={`step-container  not-in-desktop ${getClassName(step.status)}`}>
+                <div className="step">{step.title}</div>
+                {!isLastStep(step) && !isOnlyStep() && <hr className="line" />}
+              </div>
+            ))}
       </>
     );
   };
 
   return (
-    <div className="container">
+    <div className="container ">
       {props.steps.map((step, index) => (
         <div
           key={index}
-          className={`step-container not-in-mobile ${getClassName(
-            step.status,
-          )}`}>
+          className={`step-container ${getClassName(step.status)} not-in-mobile`}>
           <div className="step">{step.title}</div>
           {!isLastStep(step) && !isOnlyStep() && <hr className="line" />}
         </div>
       ))}
-      {props.steps.length >= 3 && renderMobileStepper()}
+      {renderMobileStepper()}
     </div>
   );
 };
